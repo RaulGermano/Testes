8d9288a2f0b09f7c841ee98e1618d788fcb65dd8
RaulGermano <raul.germano@icloud.com>
2019-08-31 11:56:12 -0300

    added hash files name

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 
 const server = express();
 
-const upload = multer({ dest: 'uploads/images/' });
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads/images/');
+	},
+	filename: (req, file, cb) => {
+		// To use the real file name.
+		// cb(null, file.originalname);
+
+		// To use the real file name with a 'hash'.
+		cb(
+			null,
+			`${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`
+		);
+	}
+});
+
+const upload = multer({ storage });
 
 server.use(express.static('public'));
 
-server.post('/file/upload', upload.single('file'), (req, res) => {
+server.post('/file/upload', upload.single('file-image-profile'), (req, res) => {
 	res.send('File sent!');
 });
 
-server.listen(3000);
+server.listen(3333);
